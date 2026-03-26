2c5876608ca05e7781653cdd720ee875e857b9c5
Carlito033 <engocarl03@gmail.com>
2026-03-27 02:29:52 +0800

    remove unused card component from home page

--- frontend/home.php
+++ frontend/home.php
@@ -23,15 +23,6 @@ if ($buttonClicked) {
     <form method="POST" action="">
         <input type="submit" name="logout" value="Logout">
     </form>
-
-    <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-    </div>
 </body>
 
 </html>
